2605ed6660e988f85e7335c57e9bf0dc03064a0c
ichie chinemrem richard <chinemeremichie@gmail.com>
2023-05-15 17:05:53 +0100

    second commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import "../../assets/css/footer.css"
+import FooterLogo from "../../assets/images/avvic_logo.png"
+
+export class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <footer className="footer">
+            <section>
+                <aside>
+                    <a href="#">Create account</a>
+                    <a href="#">The team</a></aside><aside>
+                        <a href="#">Policies</a><a href="">Terms and Conditions</a>
+                        <a href="#">Privacy</a></aside><aside><a href="#">FAQ</a>
+                        <a href="">Join the force</a><a href="">Our Mission</a>
+                        </aside>
+                        <aside>
+                            <address>
+                                <h1>Address</h1>
+                                <p>House B1, Rose Gardens Courts, <br />Rangers Avenue, Enugu. Nigeria</p>
+                                </address><br />You can mail us at;<a href="mailto:info@avvicgroup.com" className="mail">info@avvicgroup.com</a></aside>
+                                </section>
+                                <div className="below"><a className="image" href="/"><img src={FooterLogo} alt="" title="avvic logo" className='image' /></a>
+                                <p>Avvic Banking is a subsidiary of Avvic Groups, which is aimed towards providing services <br />in various sectors to help solve problems and profer easier methods to go about daily businesses
+                                 <br />without having to put you in a grind. </p>
+                                 <br /><p>2023. All rights are reserved by the Avvic group, and shall not be used without express permision</p>
+                                 </div>
+                                 </footer>
+      </div>
+    )
+  }
+}
+
+export default Footer
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { menuData } from './MenuData'
+import "../../assets/css/Navbar.css"
+import Logo from "../../assets/images/avvic_logo_22.png"
+
+export class Navbar extends Component {
+    state = {clicked: false};
+    handleClick = ()=>{
+        this.setState({clicked: !this.state.clicked})
+    }
+  render() {
+    return (
+   <nav className='NavbarItems'>
+    <div className='logo'>
+        <img src={Logo} alt="logo icon" className='logo-image'></img>
+    </div>
+    <div className='menu-icons' onClick={this.handleClick}> <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i></div>
+    <ul className={this.state.clicked? "nav-menu active" : "nav-menu"}>
+        {menuData.map((item, index)=>{
+            return(
+                <li key={index}>
+            <a href={item.url} className={item.cName}>
+                <i className={item.icon}></i>{item.title}
+            </a>
+        </li>
+            )
+        })}
+       
+    </ul>
+   </nav>
+    )
+  }
+}
+
+export default Navbar
